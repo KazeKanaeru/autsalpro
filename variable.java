c25c5772898d7aa13294811046f4c0852143401f
KazeKanaeru <115127474+KazeKanaeru@users.noreply.github.com>
2022-11-24 11:38:46 +0700

    Create variable.java

---
+++ variable.java
@@ -0,0 +1,11 @@
+public class Main 
+{
+    public static void main(String[] args) {
+        String nama = "Agung Restu Ramadhan";
+        int umur = 2004;
+        
+        System.out.println("Assalamualaikum");
+        System.out.println("Nama saya :"+ nama);
+        System.out.println("Saya lahir ditahun :"+ umur);
+    }
+}
